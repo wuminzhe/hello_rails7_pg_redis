0552aeae1261c774846d7fa48b66d1884889d20d
Aki Wu <wuminzhe@gmail.com>
2023-04-25 08:55:53 +0800

    rm init.sh

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "source 'https://rubygems.org'" > Gemfile  
-echo "gem 'rails', '~> 7.0.0'" >> Gemfile
-# generate Gemfile.lock
-docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ruby:3.1 bundle install
